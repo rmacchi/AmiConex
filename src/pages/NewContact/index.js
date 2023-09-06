b89f42a82686795bf160d687ace8d9a5543a7b7f
rmacchi <rafaelmacchi199@gmail.com>
2023-09-06 12:10:44 -0300

    styled tab of 'Novo Contato', created Input and Select area

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,3 +1,22 @@
+import PageHeader from "../../components/PageHeader";
+
+import Input from "../../components/Input";
+import Select from "../../components/Select";
+
 export default function NewContact() {
-  return "NewContactpage";
+  return (
+    <>
+      <PageHeader
+        title="Novo Contato"
+      />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Facebook</option>
+        <option value="123">Twitter / X</option>
+        <option value="123">LinkedIn</option>
+      </Select>
+    </>
+  );
 }

7a8be5f2639d9cacc4f3f0e54bb5dcd22a4c797a
rmacchi <rafaelmacchi199@gmail.com>
2023-09-06 10:23:06 -0300

    refactoring ContactsList and Header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,87 @@
+import {
+  Container, InputSearchContainer, Header, ListContainer, Card,
+} from "./styles";
+
+import arrow from "../../assets/images/icons/arrow.svg";
+import edit from "../../assets/images/icons/edit.svg";
+import trash from "../../assets/images/icons/trash.svg";
+
 export default function Home() {
-  return "Homepage";
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquise pelo nome..." />
+      </InputSearchContainer>
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Rafael Macchi</strong>
+              <small>instagram</small>
+            </div>
+            <span>rmacchi@devacademy.com.br</span>
+            <span>(19) 99999-99999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Carol Soares</strong>
+              <small>instagram</small>
+            </div>
+            <span>casoares@devacademy.com.br</span>
+            <span>(19) 99999-99999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Victor Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>vicsilva@devacademy.com.br</span>
+            <span>(19) 99999-99999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
 }

eace7e3da7014fe3b58f719ac66b7998344b8a35
rmacchi <rafaelmacchi199@gmail.com>
2023-09-12 02:19:50 -0300

    UX improvement with loading status

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import {
   Container, InputSearchContainer, Header, ListHeader, Card,
 } from "./styles";
@@ -9,26 +9,38 @@ import arrow from "../../assets/images/icons/arrow.svg";
 import edit from "../../assets/images/icons/edit.svg";
 import trash from "../../assets/images/icons/trash.svg";
 
-// import Loader from "../../components/Loader";
+import delay from "../../utils/delay";
+
+import Loader from "../../components/Loader";
 // import Modal from "../../components/Modal";
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
   const [orderBy, setOrderName] = useState("asc");
   const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
-  const filteredContacts = contacts.filter((contact) => (
+  const filteredContacts = useMemo(() => contacts.filter((contact) => (
     contact.name.toLowerCase().includes(searchTerm.toLowerCase())
-  ));
+  )), [contacts, searchTerm]);
 
   useEffect(() => {
+    setIsLoading(true);
+
     fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
       .then(async (response) => {
+        await delay(500);
+
         const json = await response.json();
         setContacts(json);
+        setIsLoading(false);
       });
-    try {} catch (error) {
+    try {
+
+    } catch (error) {
       console.log("erro", error);
+    } finally {
+    //   setIsLoading(false);
     }
   }, [orderBy]);
 
@@ -44,7 +56,7 @@ export default function Home() {
 
   return (
     <Container>
-      {/* <Loader /> */}
+      <Loader isLoading={isLoading} />
       {/* <Modal danger /> */}
       <InputSearchContainer>
         <input

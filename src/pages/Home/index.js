97359247e57de2fb5d963f17857970935f0092dd
rmacchi <rafaelmacchi199@gmail.com>
2023-09-12 17:43:14 -0300

    performing error handling using async/await

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -25,23 +25,26 @@ export default function Home() {
   )), [contacts, searchTerm]);
 
   useEffect(() => {
-    setIsLoading(true);
+    async function loadContacts() {
+      try {
+        setIsLoading(true);
 
-    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
-      .then(async (response) => {
+        const response = await fetch(
+          `http://localhost:3001/contacts?orderBy=${orderBy}`,
+        );
+
         await delay(500);
 
         const json = await response.json();
         setContacts(json);
+      } catch (error) {
+        console.log("erro", error);
+      } finally {
         setIsLoading(false);
-      });
-    try {
-
-    } catch (error) {
-      console.log("erro", error);
-    } finally {
-    //   setIsLoading(false);
+      }
     }
+
+    loadContacts();
   }, [orderBy]);
 
   function handleToggleOrderBy() {

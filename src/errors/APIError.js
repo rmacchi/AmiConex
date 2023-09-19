38cddb2c7ced06a7ace74950cca8bf00fa2b3b52
rmacchi <rafaelmacchi199@gmail.com>
2023-09-18 23:16:05 -0300

    created APIError

---
+++ src/errors/APIError.js
@@ -0,0 +1,12 @@
+export default class APIError extends Error {
+  constructor(response, body) {
+    super(); // Error.constructor
+
+    this.name = "APIError";
+    this.response = response;
+    this.body = body;
+    this.message = (
+      body?.error || `${response.status} - ${response.statusText}`
+    );
+  }
+}
--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -6,7 +6,7 @@ class ContactsService {
   }
 
   async listContacts(orderBy = "asc") {
-    return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
+    return this.httpClient.get(`/contacts/22e7f2f3-8d3f-4ebf-be2f-561b4893d25f?orderBy=${orderBy}`);
   }
 
   async createContacts(contact) {

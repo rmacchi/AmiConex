993f0786e9a5747d7ad7a7727ecfcb5fb4543629
rmacchi <rafaelmacchi199@gmail.com>
2023-09-21 00:55:04 -0300

    implementing contact creation

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -14,7 +14,7 @@ import Input from "../Input";
 import Select from "../Select";
 import Button from "../Button";
 
-export default function ContactForm({ buttonLabel }) {
+export default function ContactForm({ buttonLabel, onSubmit }) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
@@ -72,7 +72,7 @@ export default function ContactForm({ buttonLabel }) {
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log({
+    onSubmit({
       name, email, phone, categoryId,
     });
   }
@@ -135,4 +135,5 @@ export default function ContactForm({ buttonLabel }) {
 
 ContactForm.propTypes = {
   buttonLabel: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,24 @@
 import PageHeader from "../../components/PageHeader";
 import ContactForm from "../../components/ContactForm";
+import ContactsService from "../../services/ContactsService";
 
 export default function NewContact() {
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      const response = await ContactsService.createContacts(contact);
+
+      console.log(response);
+    } catch {
+      alert("Ocorreu um erro ao cadastrar o contato!");
+    }
+  }
   return (
     <>
       <PageHeader
@@ -10,6 +27,7 @@ export default function NewContact() {
 
       <ContactForm
         buttonLabel="Cadastrar"
+        onSubmit={handleSubmit}
       />
     </>
   );

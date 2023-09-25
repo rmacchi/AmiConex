13569d8292043666eb88f73c235b12de4da93d2d
rmacchi <rafaelmacchi199@gmail.com>
2023-09-25 16:37:19 -0300

    creating categoryMapper and including toDomain

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -39,7 +39,7 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
       setName(contact.name ?? "");
       setEmail(contact.email ?? "");
       setPhone(formatPhone(contact.phone ?? ""));
-      setCategoryId(contact.contact_id ?? "");
+      setCategoryId(contact.category.id ?? "");
     },
     resetFields: () => {
       setName("");
---
+++ src/services/mappers/ContactMapper.js
@@ -0,0 +1,25 @@
+class ContactMapper {
+  toPersistance(domainContact) {
+    return {
+      name: domainContact.name,
+      email: domainContact.email,
+      phone: domainContact.phone,
+      category_id: domainContact.categoryId,
+    };
+  }
+
+  toDomain(persistanceContact) {
+    return {
+      id: persistanceContact.id,
+      name: persistanceContact.name,
+      email: persistanceContact.email,
+      phone: persistanceContact.phone,
+      category: {
+        id: persistanceContact.category_id,
+        name: persistanceContact.category_name,
+      },
+    };
+  }
+}
+
+export default new ContactMapper();
